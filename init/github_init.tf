70aa632d007be2d9d38ce95dd6bd2cb16249bd63
Cameron Delano <knowbase@knowbase.dev>
2024-04-30 08:07:53 -0700

    dev

--- init/github_init.tf
+++ init/github_init.tf
@@ -1,39 +1,44 @@
-resource "github_actions_secret" "xc-key" {
-  repository  = "spicy-octopod"
-  secret_name = "XC_KEY"
-}
-resource "github_actions_secret" "xc-key-pass" {
-  repository  = "spicy-octopod"
-  secret_name = "XC_KEY_PASS"
-}
 resource "github_repository_environment" "env" {
   repository  = "spicy-octopod"
-  environment = format("%s-gh-env-%s-%s", var.platform, var.usecase, random_id.build_suffix.hex)
+  environment = format("%s-gh-env-%s-%s", var.usecase, var.platform, random_id.build_suffix.hex)
 }
-resource "github_actions_environment_variable {
-  repository = "spicy-octopod"
+resource "github_actions_environment_variable" "platform" {
+  repository    = "spicy-octopod"
+  environment   = github_repository_environment.env.environment
+  variable_name = "PLATFORM"
+  value         = var.platform
+}
+resource "github_actions_environment_variable" "build_suffix" {
+  repository    = "spicy-octopod"
+  environment   = github_repository_environment.env.environment
+  variable_name = "BUILD_SUFFIX"
+  value         = random_id.build_suffix.hex
+}
+resource "github_actions_environment_secret" "xc-key" {
+  repository  = "spicy-octopod"
   environment = github_repository_environment.env.environment
-  variable_name = platform
-  value = var.platform
+  secret_name = "XC_KEY"
 }
-resource "github_actions_environment_variable {
-  repository = "spicy-octopod"
+resource "github_actions_environment_secret" "xc-key-pass" {
+  repository  = "spicy-octopod"
   environment = github_repository_environment.env.environment
-  variable_name = build_suffix
-  value = random_id.build_suffix.hex
+  secret_name = "XC_KEY_PASS"
 }
-resource "github_actions_secret" "aws-access-key-id" {
+resource "github_actions_environment_secret" "aws-access-key-id" {
   count       = var.platform == "aws" ? 1 : 0
   repository  = "spicy-octopod"
+  environment = github_repository_environment.env.environment
   secret_name = "AWS_ACCESS_KEY_ID"
 }
-resource "github_actions_secret" "aws-secret-access-key" {
+resource "github_actions_environment_secret" "aws-secret-access-key" {
   count       = var.platform == "aws" ? 1 : 0
   repository  = "spicy-octopod"
+  environment = github_repository_environment.env.environment
   secret_name = "AWS_SECRET_ACCESS_KEY"
 }
-resource "github_actions_secret" "aws-session-token" {
+resource "github_actions_environment_secret" "aws-session-token" {
   count       = var.platform == "aws" ? 1 : 0
   repository  = "spicy-octopod"
+  environment = github_repository_environment.env.environment
   secret_name = "AWS_SESSION_TOKEN"
 }
